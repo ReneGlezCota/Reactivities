4f54c86e883189c9922f4e95893cbe7cf7071eb1
ReneGlezCota <reneglezcota@gmail.com>
2022-07-28 17:11:05 -0700

    End of section 9

--- client-app/src/app/stores/activityStores.ts
+++ client-app/src/app/stores/activityStores.ts
@@ -5,7 +5,7 @@ import { Activity } from "../models/activity";
 export default class ActivityStore {
   activityRegistry = new Map<string, Activity>();
   selectedActivity: Activity | undefined = undefined;
-  editMode = false; 
+  editMode = false;
   loading = false;
   loadingInitial = true;
 
@@ -24,6 +24,18 @@ export default class ActivityStore {
   get activitiesByDate() {
     return Array.from(this.activityRegistry.values()).sort(
       (a, b) => Date.parse(a.date) - Date.parse(b.date)
+    );
+  }
+
+  get groupedActivities() {
+    return Object.entries(
+      this.activitiesByDate.reduce((activities, activity) => {
+        const date = activity.date;
+        activities[date] = activities[date]
+          ? [...activities[date], activity]
+          : [activity];
+        return activities;
+      }, {} as { [key: string]: Activity[] })
     );
   }
 
